48c3cc36864046fa44b4c4483d70b5e51e28a481
Baur Urazalinov <burazalinov@signifyhealth.com>
2022-05-22 20:30:34 -0500

    Fixed test Selenium version increased

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -3,22 +3,23 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
+
 class TestBrowser(unittest.TestCase):
-  def setUp(self) -> None:
+    def setUp(self) -> None:
         """
         Initiate driver for each test
         """
         service = Service(ChromeDriverManager().install())
         self.driver = webdriver.Chrome(service=service)
-   
-  def test_navigate_to_google(self) -> None:
+
+    def test_navigate_to_google(self) -> None:
         """
         Navigate to Google
         """
         self.driver.get('https://www.google.com')
         self.assertEqual(self.driver.title, 'Google')
-  
-   def tearDown(self) -> None:
+
+    def tearDown(self) -> None:
         """
         Closing driver after each test
         """
