8156074f0bd3d47daf725afe63c7cc4e193bfca7
Baur Urazalinov <BurhanH@users.noreply.github.com>
2022-05-22 21:41:55 -0500

    Update test_dummy.py

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,7 +1,6 @@
 import unittest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
 from selenium.webdriver.chrome.service import Service
 
 
@@ -10,21 +9,8 @@ class TestBrowser(unittest.TestCase):
         """
         Initiate driver for each test
         """
-        service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-        chrome_options = Options()
-        options = [
-            "--headless",
-            "--disable-gpu",
-            "--window-size=1920,1200",
-            "--ignore-certificate-errors",
-            "--disable-extensions",
-            "--no-sandbox",
-            "--disable-dev-shm-usage"
-        ]
-        for option in options:
-            chrome_options.add_argument(option)
-        
-        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+        service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service)
 
     def test_navigate_to_google(self) -> None:
         """
