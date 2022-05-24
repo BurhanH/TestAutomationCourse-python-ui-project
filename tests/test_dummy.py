9baaedd5adf19c682f284bedc7c4f71b2b24ae64
Baur Urazalinov <BurhanH@users.noreply.github.com>
2022-05-23 21:20:24 -0500

    Update test_dummy.py

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,16 +12,12 @@ class TestBrowser(unittest.TestCase):
         """
         service = Service(ChromeDriverManager().install())
         chrome_options = webdriver.ChromeOptions()
-        # --disable-gpu;
-        # --no-sandbox;
-        # --disable-dev-shm-usage;
-        # --headless;
-        # --window-size=1920,1080
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--window-size=1920,1080')
+        if 'CICD_RUN' in os.environ:
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-gpu')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--window-size=1920,1080')
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
 
     def test_navigate_to_google(self) -> None:
