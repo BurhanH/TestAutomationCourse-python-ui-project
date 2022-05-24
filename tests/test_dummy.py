bce2fbcedac014ba886411d2c5ad67c5ce5c639c
Baur Urazalinov <BurhanH@users.noreply.github.com>
2022-05-23 21:22:21 -0500

    Burhan h patch 1 (#2)
    * Create test_dummy.py
    
    * Create requirements.txt
    
    * Create run_tests.yml (#1)
    
    * Update run_tests.yml
    
    * Update run_tests.yml
    
    * Update run_tests.yml
    
    * Fixed test
    Selenium version increased
    
    * Update run_tests.yml
    
    * Update run_tests.yml
    
    * Update run_tests.yml
    
    * Update test_dummy.py
    
    * Update run_tests.yml
    
    * Update run_tests.yml
    
    * Update test_dummy.py
    
    * Update run_tests.yml
    
    * Update test_dummy.py
    
    * Update run_tests.yml
    
    * Update test_dummy.py
    
    * Update run_tests.yml
    
    Co-authored-by: Baur Urazalinov <burazalinov@signifyhealth.com>

---
+++ tests/test_dummy.py
@@ -0,0 +1,38 @@
+import os
+import unittest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+
+class TestBrowser(unittest.TestCase):
+    def setUp(self) -> None:
+        """
+        Initiate driver for each test
+        """
+        service = Service(ChromeDriverManager().install())
+        chrome_options = webdriver.ChromeOptions()
+        if 'CICD_RUN' in os.environ:
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-gpu')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--window-size=1920,1080')
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    def test_navigate_to_google(self) -> None:
+        """
+        Navigate to Google
+        """
+        self.driver.get('https://www.google.com')
+        self.assertEqual(self.driver.title, 'Google')
+
+    def tearDown(self) -> None:
+        """
+        Closing driver after each test
+        """
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
