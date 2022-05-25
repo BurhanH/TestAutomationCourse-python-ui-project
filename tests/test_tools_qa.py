96353d4d5b1a862a9c001bca317ae30d7bd088f7
Stan Aretinskiy <sr575sr@gmail.com>
2022-05-25 12:55:16 -0700

    SA-RF :: Refactor test_tools_qa (#7)
    * Improve test suite setup
    
    * Addition to .gitignore
    
    * Refactor test_tools_qa.py
     - rename class
     - replace locators
     - remove hard waits
     - fix test_books_link
     - improve style

--- tests/test_tools_qa.py
+++ tests/test_tools_qa.py
@@ -1,84 +1,69 @@
 import os
-import time
 import unittest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+BASE_URL = 'https://demoqa.com'
 
 
-class TestBrowser(unittest.TestCase):
+class TestToolsQANavigation(unittest.TestCase):
     def setUp(self) -> None:
-        """
-        Initiate driver for each test
-        """
         service = Service(ChromeDriverManager().install())
         chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--window-size=1920,1080')
         if 'CICD_RUN' in os.environ:
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-gpu')
             chrome_options.add_argument('--disable-dev-shm-usage')
             chrome_options.add_argument('--headless')
-            chrome_options.add_argument('--window-size=1920,1080')
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
+        self.wait = WebDriverWait(self.driver, 8)
+        self.driver.get(BASE_URL)
 
     def test_navigate_to_toolsQA(self):
-        self.driver.get('https://demoqa.com')
         self.assertEqual(self.driver.title, 'ToolsQA')
 
     def test_logo(self):
-        self.driver.get('https://demoqa.com')
-        self.driver.find_element(By.XPATH, '(//*[@id="app"]/header/a/img)')
+        logo_image = self.driver.find_element(By.XPATH, '//header//img')
+        self.assertTrue(logo_image.is_displayed())
 
-    def test_element_link(self):
-        self.driver.get('https://demoqa.com')
-        element = self.driver.find_element(By.XPATH, '(/html/body/div[2]/div/div/div[2]/div/div[1]/div/div[2])')
-        element.click()
-        time.sleep(8)
-        self.assertEqual(self.driver.current_url, 'https://demoqa.com/elements')
+    def test_elements_link(self):
+        elements_card = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//h5[text()='Elements']")))
+        elements_card.click()
+        self.assertEqual(self.driver.current_url, f'{BASE_URL}/elements')
 
-    def test_form_link(self):
-        self.driver.get('https://demoqa.com')
-        form = self.driver.find_element(By.XPATH, '(//*[@id="app"]/div/div/div[2]/div/div[2]/div/div[2])')
-        form.click()
-        time.sleep(8)
-        self.assertEqual(self.driver.current_url, 'https://demoqa.com/forms')
+    def test_forms_link(self):
+        forms_card = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//h5[text()='Forms']")))
+        forms_card.click()
+        self.assertEqual(self.driver.current_url, f'{BASE_URL}/forms')
 
     def test_alerts_link(self):
-        self.driver.get('https://demoqa.com')
-        alerts = self.driver.find_element(By.XPATH, '(//*[@id="app"]/div/div/div[2]/div/div[3]/div/div[2])')
-        alerts.click()
-        time.sleep(8)
-        self.assertEqual(self.driver.current_url, 'https://demoqa.com/alertsWindows')
+        alerts_card = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//h5[contains(text(), 'Alerts')]")))
+        alerts_card.click()
+        self.assertEqual(self.driver.current_url, f'{BASE_URL}/alertsWindows')
 
     def test_widgets_link(self):
-        self.driver.get('https://demoqa.com')
-        widgets = self.driver.find_element(By.XPATH, '(//*[@id="app"]/div/div/div[2]/div/div[4]/div/div[2])')
-        widgets.click()
-        time.sleep(8)
-        self.assertEqual(self.driver.current_url, 'https://demoqa.com/widgets')
+        widgets_card = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//h5[text()='Widgets']")))
+        widgets_card.click()
+        self.assertEqual(self.driver.current_url, f'{BASE_URL}/widgets')
 
     def test_interactions_link(self):
-        self.driver.get('https://demoqa.com')
-        interactions = self.driver.find_element(By.XPATH, '(//*[@id="app"]/div/div/div[2]/div/div[5]/div/div[2])')
-        interactions.click()
-        time.sleep(8)
-        self.assertEqual(self.driver.current_url, 'https://demoqa.com/interaction')
+        interactions_card = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//h5[text()='Interactions']")))
+        interactions_card.click()
+        self.assertEqual(self.driver.current_url, f'{BASE_URL}/interaction')
 
-    @unittest.skip ("need fix")
     def test_books_link(self):
-        self.driver.get('https://demoqa.com')
-        books = self.driver.find_element(By.XPATH, '(//*[@id="app"]/div/div/div[2]/div/div[6]/div/div[2])')
-        books.click()
-        time.sleep(8)
-        self.assertEqual(self.driver.current_url, 'https://demoqa.com/books')
+        books_card = self.wait.until(EC.visibility_of_element_located((By.XPATH, "//h5[contains(text(), 'Book')]")))
+        self.driver.execute_script(f"window.scrollTo(0, {self.driver.get_window_size().get('height')})")
+        books_card.click()
+        self.assertEqual(self.driver.current_url, f'{BASE_URL}/books')
 
     def tearDown(self) -> None:
-        """
-        Closing driver after each test
-        """
         self.driver.quit()
-
 
 
 if __name__ == '__main__':
