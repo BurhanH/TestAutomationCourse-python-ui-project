7ff3c2a0f2f82385fe49ddbaf3d734ef3f1ac113
Stan Aretinskiy <sr575sr@gmail.com>
2022-05-28 14:40:03 -0700

    Refactor tests for alerts

--- tests/test_toolsqa_alerts.py
+++ tests/test_toolsqa_alerts.py
@@ -11,6 +11,12 @@ from selenium.common.exceptions import UnexpectedAlertPresentException, TimeoutE
     NoSuchElementException
 
 BASE_URL = 'https://demoqa.com/alerts'
+INSTANT_ALERT_BUTTON_ID = 'alertButton'
+DELAYED_ALERT_BUTTON_ID = 'timerAlertButton'
+CONFIRMATION_ALERT_BUTTON_ID = 'confirmButton'
+PROMPT_ALERT_BUTTON_ID = 'promtButton'
+CONFIRMATION_RESULT_ID = 'confirmResult'
+PROMPT_RESULT_ID = 'promptResult'
 
 
 class TestToolsQAAlerts(unittest.TestCase):
@@ -30,9 +36,8 @@ class TestToolsQAAlerts(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    @unittest.skip
     def test_alert(self):
-        self.driver.find_element(By.ID, 'alertButton').click()
+        self.driver.find_element(By.ID, INSTANT_ALERT_BUTTON_ID).click()
 
         try:
             alert = self.driver.switch_to.alert
@@ -43,18 +48,17 @@ class TestToolsQAAlerts(unittest.TestCase):
         self.assertEqual(actual_alert_text, expected_alert_text,
                          f"Unexpected alert text: '{actual_alert_text}', expected: '{expected_alert_text}'.")
 
-    @unittest.skip
     def test_delayed_alert(self):
-        self.driver.find_element(By.ID, 'timerAlertButton').click()
+        self.driver.find_element(By.ID, DELAYED_ALERT_BUTTON_ID).click()
 
         start_time = time.time()
         try:
-            self.wait.until(EC.alert_is_present())
+            alert = self.wait.until(EC.alert_is_present())
+        except UnexpectedAlertPresentException:
             alert = self.driver.switch_to.alert
         except TimeoutException:
             self.fail("Delayed alert didn't appear in 10 seconds.")
-        except UnexpectedAlertPresentException:
-            alert = self.driver.switch_to.alert
+
         actual_alert_text = alert.text
         delay = time.time() - start_time
         expected_alert_text = 'This alert appeared after 5 seconds'
@@ -63,126 +67,116 @@ class TestToolsQAAlerts(unittest.TestCase):
         self.assertAlmostEqual(delay, 5, delta=0.3, msg=f"Alert appeared in {delay} sec, expected in 5 sec.")
 
     def test_confirmation_alert_accept(self):
-        self.driver.find_element(By.ID, 'confirmButton').click()
+        self.driver.find_element(By.ID, CONFIRMATION_ALERT_BUTTON_ID).click()
 
         try:
             alert = self.driver.switch_to.alert
         except NoAlertPresentException:
-            self.fail('Alert is not present after click on third button.')
-        actual_alert_text = alert.text
+            self.fail('Confirmation alert is not present after click on third button.')
+        actual_confirmation_alert_text = alert.text
         expected_alert_text = 'Do you confirm action?'
-        self.assertEqual(actual_alert_text, expected_alert_text,
-                         f"Unexpected alert text: '{actual_alert_text}', expected: '{expected_alert_text}'.")
+        self.assertEqual(actual_confirmation_alert_text, expected_alert_text,
+                         f"Unexpected alert text: '{actual_confirmation_alert_text}', "
+                         f"expected: '{expected_alert_text}'.")
 
         alert.accept()
-        confirm_result = self.driver.find_element(By. ID, 'confirmResult')
-        actual_alert_text = confirm_result.text
-        expected_alert_text = 'You selected Ok'
-        self.assertEqual(actual_alert_text, expected_alert_text,
-                         f"Unexpected confirm result text: '{actual_alert_text}', expected: '{expected_alert_text}'.")
+
+        actual_confirmation_result_text = self.driver.find_element(By.ID, CONFIRMATION_RESULT_ID).text
+        expected_confirmation_text = 'You selected Ok'
+        self.assertEqual(actual_confirmation_result_text, expected_confirmation_text,
+                         f"Unexpected confirmation result text: '{actual_confirmation_result_text}', "
+                         f"expected: '{expected_confirmation_text}'.")
 
     def test_confirmation_alert_cancel(self):
-        self.driver.find_element(By.ID, 'confirmButton').click()
+        self.driver.find_element(By.ID, CONFIRMATION_ALERT_BUTTON_ID).click()
 
         try:
             alert = self.driver.switch_to.alert
         except NoAlertPresentException:
-            self.fail('Alert is not present after click on third button.')
-        actual_alert_text = alert.text
+            self.fail('Confirmation alert is not present after click on third button.')
+        actual_confirmation_alert_text = alert.text
         expected_alert_text = 'Do you confirm action?'
-        self.assertEqual(actual_alert_text, expected_alert_text,
-                         f"Unexpected alert text: '{actual_alert_text}', expected: '{expected_alert_text}'.")
+        self.assertEqual(actual_confirmation_alert_text, expected_alert_text,
+                         f"Unexpected alert text: '{actual_confirmation_alert_text}', "
+                         f"expected: '{expected_alert_text}'.")
 
         alert.dismiss()
-        confirm_result = self.driver.find_element(By. ID, 'confirmResult')
-        actual_alert_text = confirm_result.text
-        expected_alert_text = 'You selected Cancel'
-        self.assertEqual(actual_alert_text, expected_alert_text,
-                         f"Unexpected confirm result text: '{actual_alert_text}', expected: '{expected_alert_text}'.")
 
+        actual_confirmation_result_text = self.driver.find_element(By.ID, CONFIRMATION_RESULT_ID).text
+        expected_confirmation_text = 'You selected Cancel'
+        self.assertEqual(actual_confirmation_result_text, expected_confirmation_text,
+                         f"Unexpected confirmation result text: '{actual_confirmation_result_text}', "
+                         f"expected: '{expected_confirmation_text}'.")
 
     def test_prompt_alert_cancel(self):
-        self.driver.find_element(By.ID,'promtButton').click()
+        self.driver.find_element(By.ID, PROMPT_ALERT_BUTTON_ID).click()
 
         try:
             alert = self.driver.switch_to.alert
         except NoAlertPresentException:
-            self.fail('Alert is not present after click on fourth button.')
-        actual_alert_text = alert.text
-        expected_alert_text = 'Please enter your name'
-        self.assertEqual(actual_alert_text, expected_alert_text,
-                         f"Unexpected alert text: '{actual_alert_text}', expected: '{expected_alert_text}'.")
+            self.fail('Prompt alert is not present after click on fourth button.')
+        actual_prompt_result_text = alert.text
+        expected_prompt_result_text = 'Please enter your name'
+        self.assertEqual(actual_prompt_result_text, expected_prompt_result_text,
+                         f"Unexpected alert text: '{actual_prompt_result_text}', "
+                         f"expected: '{expected_prompt_result_text}'.")
 
         alert.dismiss()
         try:
-            prompt_result = self.driver.find_element(By. ID, 'promptResult')
-            self.fail(f"Text {prompt_result.text} should not be present! Call 911 =)")
+            prompt_result_text = self.driver.find_element(By.ID, PROMPT_RESULT_ID).text
+            self.fail(f"Unexpected text as a result of dismissed prompt alert: '{prompt_result_text}'.")
         except NoSuchElementException:
             pass
 
     def test_prompt_alert_fill_out_field_cancel(self):
-        self.driver.find_element(By.ID,'promtButton').click()
+        self.driver.find_element(By.ID, PROMPT_ALERT_BUTTON_ID).click()
 
         try:
             alert = self.driver.switch_to.alert
         except NoAlertPresentException:
-            self.fail('Alert is not present after click on fourth button.')
-        actual_alert_text = alert.text
-        expected_alert_text = 'Please enter your name'
-        self.assertEqual(actual_alert_text, expected_alert_text,
-                         f"Unexpected alert text: '{actual_alert_text}', expected: '{expected_alert_text}'.")
+            self.fail('Prompt alert is not present after click on fourth button.')
 
         alert.send_keys('something!')
-
         alert.dismiss()
         try:
-            prompt_result = self.driver.find_element(By. ID, 'promptResult')
-            self.fail(f"Text {prompt_result.text} should not be present! Call 911 =)")
+            prompt_result_text = self.driver.find_element(By.ID, PROMPT_RESULT_ID).text
+            self.fail(f"Unexpected text as a result of dismissed prompt alert: '{prompt_result_text}'.")
         except NoSuchElementException:
             pass
 
     def test_prompt_alert_empty_field_ok(self):
-        self.driver.find_element(By.ID,'promtButton').click()
+        self.driver.find_element(By.ID, PROMPT_ALERT_BUTTON_ID).click()
 
         try:
             alert = self.driver.switch_to.alert
         except NoAlertPresentException:
-            self.fail('Alert is not present after click on fourth button.')
-        actual_alert_text = alert.text
-        expected_alert_text = 'Please enter your name'
-        self.assertEqual(actual_alert_text, expected_alert_text,
-                         f"Unexpected alert text: '{actual_alert_text}', expected: '{expected_alert_text}'.")
+            self.fail('Prompt alert is not present after click on fourth button.')
 
         alert.accept()
         try:
-            prompt_result = self.driver.find_element(By. ID, 'promptResult')
-            self.fail(f"Text {prompt_result.text} should not be present! Call 911 =)")
+            prompt_result_text = self.driver.find_element(By.ID, PROMPT_RESULT_ID).text
+            self.fail(f"Unexpected text as a result of submitted empty prompt alert: '{prompt_result_text}'.")
         except NoSuchElementException:
             pass
 
     def test_prompt_alert_fill_out_field_ok(self):
-        self.driver.find_element(By.ID,'promtButton').click()
+        name = 'Elinor Mcmanus'
+
+        self.driver.find_element(By.ID, PROMPT_ALERT_BUTTON_ID).click()
 
         try:
             alert = self.driver.switch_to.alert
         except NoAlertPresentException:
-            self.fail('Alert is not present after click on fourth button.')
+            self.fail('Prompt alert is not present after click on fourth button.')
 
-        actual_alert_text = alert.text
-        expected_alert_text = 'Please enter your name'
-        self.assertEqual(actual_alert_text, expected_alert_text,
-                         f"Unexpected alert text: '{actual_alert_text}', expected: '{expected_alert_text}'.")
-
-        text_name = 'Billy'
-        alert.send_keys(text_name)
-
+        alert.send_keys(name)
         alert.accept()
 
-        actual_prompt_result_text = self.driver.find_element(By. ID, 'promptResult').text
-        expected_alert_prompt_text = "You entered " + text_name
-        self.assertEqual(actual_prompt_result_text, expected_alert_prompt_text,
-                         f"Unexpected prompt result text: '{actual_prompt_result_text}', expected: '{expected_alert_prompt_text}'.")
-
+        actual_prompt_result_text = self.driver.find_element(By.ID, PROMPT_RESULT_ID).text
+        expected_prompt_result_text = f'You entered {name}'
+        self.assertEqual(actual_prompt_result_text, expected_prompt_result_text,
+                         f"Unexpected prompt result text: '{actual_prompt_result_text}', "
+                         f"expected: '{expected_prompt_result_text}'.")
 
 
 if __name__ == '__main__':
