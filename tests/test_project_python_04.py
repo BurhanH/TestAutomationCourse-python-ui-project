1ba26c5c48184f7dcb56300b7b750d7d8a92cc12
Tatiana Yurchenko <taf88156@mail.ru>
2022-05-31 03:39:01 +0300

    Изменен метод setUp

--- tests/test_project_python_04.py
+++ tests/test_project_python_04.py
@@ -6,7 +6,18 @@ from selenium.webdriver.common.by import By
 class TestPleeease(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.driver = webdriver.Chrome()
+        """
+        Initiate driver for each test
+        """
+        service = Service(ChromeDriverManager().install())
+        chrome_options = webdriver.ChromeOptions()
+        if 'CICD_RUN' in os.environ:
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-gpu')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--window-size=1920,1080')
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
 
     def test_1(self):
         self.driver.get("https://demoqa.com/")
