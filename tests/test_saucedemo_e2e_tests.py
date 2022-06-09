033c8ce6fdbacb9002d6110e66d04faa47f73388
Slava Yakovlev <dddjemy@gmail.com>
2022-06-09 11:23:08 +0300

    adding error messages to assertions and deleting useless variable

--- tests/test_saucedemo_e2e_tests.py
+++ tests/test_saucedemo_e2e_tests.py
@@ -35,8 +35,9 @@ class TestSaucedemoE2E(BaseTest):
         password_input.click()
         password_input.send_keys(CORRECT_PASSWORD)
 
-        login_button = self.driver.find_element(By.ID, LOGIN_BUTTON_LOCATOR_ID).click()
-        self.assertEqual(self.driver.current_url, F'{BASE_URL}/inventory.html')
+        self.driver.find_element(By.ID, LOGIN_BUTTON_LOCATOR_ID).click()
+        self.assertEqual(self.driver.current_url, F'{BASE_URL}/inventory.html',
+                         'Current url and expected url does not match')
 
     def test_buy_backpack(self) -> None:
         user_name_input = self.driver.find_element(By.ID, USER_NAME_LOCATOR_ID)
@@ -47,24 +48,28 @@ class TestSaucedemoE2E(BaseTest):
         password_input.click()
         password_input.send_keys(CORRECT_PASSWORD)
 
-        login_button = self.driver.find_element(By.ID, LOGIN_BUTTON_LOCATOR_ID).click()
+        self.driver.find_element(By.ID, LOGIN_BUTTON_LOCATOR_ID).click()
 
         actual_first_item_name = self.driver.find_element(By.ID, 'item_4_title_link').text
         expected_first_item_name = 'Sauce Labs Backpack'
-        self.assertEqual(actual_first_item_name, expected_first_item_name)
+        self.assertEqual(actual_first_item_name, expected_first_item_name, 'Wrong item name')
 
-        add_to_cart_button = self.driver.find_element(By.ID, 'add-to-cart-sauce-labs-backpack').click()
+        self.driver.find_element(By.ID, 'add-to-cart-sauce-labs-backpack').click()
 
         shopping_cart_badge_count = self.driver.find_element(By.XPATH, '//a/span').text
         expected_shopping_cart_badge = '1'
-        self.assertEqual(shopping_cart_badge_count, expected_shopping_cart_badge)
+        self.assertEqual(shopping_cart_badge_count, expected_shopping_cart_badge,
+                         f"Cart badge suppose to be equal, but actual badge is: '{shopping_cart_badge_count}') and "
+                         f"expected badge is: '{expected_shopping_cart_badge}' ")
 
-        shopping_cart_button = self.driver.find_element(By.ID, 'shopping_cart_container').click()
+        self.driver.find_element(By.ID, 'shopping_cart_container').click()
 
         item_title_text = self.driver.find_element(By.ID, 'item_4_title_link').text
-        self.assertEqual(item_title_text, expected_first_item_name)
+        self.assertEqual(item_title_text, expected_first_item_name,
+                         f"Unexpected title: '{item_title_text}', "
+                         f"expected: '{expected_first_item_name}'.")
 
-        checkout_button = self.driver.find_element(By.ID, 'checkout').click()
+        self.driver.find_element(By.ID, 'checkout').click()
 
         first_name_input = self.driver.find_element(By.ID, 'first-name')
         first_name_input.click()
@@ -78,13 +83,15 @@ class TestSaucedemoE2E(BaseTest):
         postal_code_input.click()
         postal_code_input.send_keys('02211')
 
-        continue_button = self.driver.find_element(By.ID, 'continue').click()
+        self.driver.find_element(By.ID, 'continue').click()
 
-        finish_him_button = self.driver.find_element(By.ID, 'finish').click()
+        self.driver.find_element(By.ID, 'finish').click()
 
         complete_message_text = self.driver.find_element(By.XPATH, '//*[@id="checkout_complete_container"]/h2').text
         expected_complete_message = 'THANK YOU FOR YOUR ORDER'
-        self.assertEqual(complete_message_text, expected_complete_message)
+        self.assertEqual(complete_message_text, expected_complete_message,
+                         f"After a successful purchase, the user should see '{expected_complete_message}',"
+                         f" but sees: '{complete_message_text}'.")
 
     def test_failed_login(self) -> None:
         user_name_input = self.driver.find_element(By.ID, USER_NAME_LOCATOR_ID)
@@ -95,11 +102,12 @@ class TestSaucedemoE2E(BaseTest):
         password_input.click()
         password_input.send_keys(WRONG_PASSWORD)
 
-        login_button = self.driver.find_element(By.ID, LOGIN_BUTTON_LOCATOR_ID).click()
+        self.driver.find_element(By.ID, LOGIN_BUTTON_LOCATOR_ID).click()
 
         actual_failed_login_text = self.driver.find_element(By.XPATH, '//form/div[3]/h3').text
         expected_failed_login_text = 'Epic sadface: Username and password do not match any user in this service'
-        self.assertEqual(actual_failed_login_text, expected_failed_login_text)
+        self.assertEqual(actual_failed_login_text, expected_failed_login_text,
+                         'Actual failed login text not equal Expected failed login text')
 
     @pytest.mark.smoke
     def test_success_log_out(self) -> None:
@@ -111,14 +119,14 @@ class TestSaucedemoE2E(BaseTest):
         password_input.click()
         password_input.send_keys(CORRECT_PASSWORD)
 
-        login_button = self.driver.find_element(By.ID, LOGIN_BUTTON_LOCATOR_ID).click()
+        self.driver.find_element(By.ID, LOGIN_BUTTON_LOCATOR_ID).click()
 
-        hamburger_menu = self.driver.find_element(By.ID, HAMBURGER_MENU_LOCATOR_ID).click()
+        self.driver.find_element(By.ID, HAMBURGER_MENU_LOCATOR_ID).click()
 
         log_out_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="logout_sidebar_link"]')))
         log_out_button.click()
 
-        login_form = self.driver.find_element(By.ID, 'login_button_container').is_displayed()
+        self.driver.find_element(By.ID, 'login_button_container').is_displayed()
 
 
 if __name__ == '__main__':
