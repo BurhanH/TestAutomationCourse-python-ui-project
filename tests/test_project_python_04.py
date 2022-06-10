b49f477a7e654ade0a303f129e1758a2f2c3a6a3
Baur Urazalinov <BurhanH@users.noreply.github.com>
2022-06-09 21:33:07 -0500

    Update test_project_python_04.py
    Fixed

--- tests/test_project_python_04.py
+++ tests/test_project_python_04.py
@@ -1,37 +1,17 @@
-import time
 import unittest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from utils.base_test import BaseTest
 
-class TestPleeease(unittest.TestCase):
+class TestPleeease(BaseTest):
 
     def setUp(self) -> None:
-        """
-        Initiate driver for each test
-        """
-        service = Service(ChromeDriverManager().install())
-        chrome_options = webdriver.ChromeOptions()
-        if 'CICD_RUN' in os.environ:
-            chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument('--disable-gpu')
-            chrome_options.add_argument('--disable-dev-shm-usage')
-            chrome_options.add_argument('--headless')
-            chrome_options.add_argument('--window-size=1920,1080')
-        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+        super(TestToolsQANavigation, self).setUp("https://demoqa.com/")
 
     def test_1(self):
-        self.driver.get("https://demoqa.com/")
         doc_link = self.driver.find_element(By.XPATH, "//h5[contains(text(),'Forms')]")
 
         doc_link.click()
         self.assertEqual(self.driver.current_url, "https://demoqa.com/forms")
-        #time.sleep(5)
-
-
-
-    def tearDown(self) -> None:
-        self.driver.close()
-
 
 
 if __name__ == '__main__':
