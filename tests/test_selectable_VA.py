8d147ce87f7af1bd13cd15d95f047222c2abf79e
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2022-06-09 20:09:09 -0500

    Update test_selectable_VA.py

--- tests/test_selectable_VA.py
+++ tests/test_selectable_VA.py
@@ -1,32 +1,49 @@
 import time
 import unittest
-
-from selenium import webdriver
 from selenium.webdriver.common.by import By
+from utils.base_test import BaseTest
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.add_argument("--start-maximized")
 
-class TestSelectable(unittest.TestCase):
+BASE_URL = 'https://demoqa.com/selectable'
 
+
+class TestSelectable(BaseTest):
     def setUp(self) -> None:
-        self.driver = webdriver.Chrome()
-        self.driver.get('https://demoqa.com/selectable')
+        self._setUp(BASE_URL)
         self.assertEqual(self.driver.current_url, "https://demoqa.com/selectable")
 
-    def test_selectable_list(self):
+    def test_selectable_header_is_displayed(self):
+        self.assertTrue(self.driver.find_element(By.CLASS_NAME, "main-header"))
+        self.assertTrue(self.driver.find_element(By.XPATH, "//div[contains(text(),'Selectable')]"))
+
+    def test_selectable_list_is_displayed(self):
         self.driver.find_element(By.XPATH, "//span[contains(text(),'Selectable')]")
+        demo_tab_list = self.driver.find_element(By.ID, "demo-tab-list")
+        self.assertTrue(demo_tab_list.is_displayed())
+
+    def test_selectable_text_is_displayed(self):
+        self.driver.find_element(By.XPATH, "//li[contains(text(),'Cras justo odio')]").is_displayed()
         self.driver.find_element(By.XPATH, "//li[contains(text(),'Cras justo odio')]").click()
+
+        self.driver.find_element(By.XPATH, "//li[contains(text(),'Dapibus ac facilisis in')]").is_displayed()
         self.driver.find_element(By.XPATH, "//li[contains(text(),'Dapibus ac facilisis in')]").click()
+
+        self.driver.find_element(By.XPATH, "//li[contains(text(),'Morbi leo risus')]").is_displayed()
         self.driver.find_element(By.XPATH, "//li[contains(text(),'Morbi leo risus')]").click()
+
+        self.driver.find_element(By.XPATH, "//li[contains(text(),'Porta ac consectetur ac')]").is_displayed()
         self.driver.find_element(By.XPATH, "//li[contains(text(),'Porta ac consectetur ac')]").click()
-        self.driver.find_element(By.ID, "demo-tab-grid").click()
-        time.sleep(2)
+
+    def test_grid_is_displayed(self):
+        demo_tab_grid = self.driver.find_element(By.ID, "demo-tab-grid")
+        self.assertTrue(demo_tab_grid.is_displayed())
 
     def test_selectable_grid(self):
-        self.driver.find_element(By.XPATH, "//li[contains(text(),'Nine')]")
-        self.driver.find_element(By.XPATH, "//li[contains(text(),'Five')]")
-        self.driver.find_element(By.XPATH, "//li[contains(text(),'One')]")
-        #driver.find_element(By.XPATH, "//img[@alt='Katalon Acedemy']").click() // trying to open this link in the end
-        #of this test but it doens't go quite as well as I expect it, need to figure it out
-        #time.sleep(2)
+        self.driver.find_element(By.XPATH, "//li[contains(text(),'Nine')]").is_displayed()
+        self.driver.find_element(By.XPATH, "//li[contains(text(),'Five')]").is_displayed()
+        self.driver.find_element(By.XPATH, "//li[contains(text(),'One')]").is_displayed()
 
     def tearDown(self) -> None:
         self.driver.close()
