4d8e77a8381b5487f685c931326a68706d10a1b7
TatianaYurchenko <102853445+TatianaYurchenko@users.noreply.github.com>
2022-06-10 05:41:49 +0300

    Сценарии для автотестов и 1 автотест (#12)
    * Сценарии для автотестов и автотест - переход по ссылке
    
    * Изменен метод setUp
    
    * Update test_project_python_04.py
    
    Fixed
    
    * Update test_project_python_04.py
    
    Co-authored-by: Baur Urazalinov <BurhanH@users.noreply.github.com>

---
+++ tests/test_project_python_04.py
@@ -0,0 +1,18 @@
+import unittest
+from selenium.webdriver.common.by import By
+from utils.base_test import BaseTest
+
+class TestPleeease(BaseTest):
+
+    def setUp(self) -> None:
+        super(TestPleeease, self).setUp("https://demoqa.com/")
+
+    def test_1(self):
+        doc_link = self.driver.find_element(By.XPATH, "//h5[contains(text(),'Forms')]")
+
+        doc_link.click()
+        self.assertEqual(self.driver.current_url, "https://demoqa.com/forms")
+
+
+if __name__ == '__main__':
+    unittest.main()
