602545b232127b4f9ae5d021fc7faafda199426d
Vittorio Altagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2022-06-13 10:03:53 -0500

    Update test_selectable_VA.py
    Deleted comments and print statements

--- tests/test_selectable_VA.py
+++ tests/test_selectable_VA.py
@@ -23,7 +23,6 @@ class TestSelectable(BaseTest):
         demo_tab_list = self.driver.find_element(By.ID, "demo-tab-list")
         self.assertTrue(demo_tab_list.is_displayed())
 
-    # includes 1st and 2nd sentences
     def test_selectable_text_is_displayed_1(self):
         list_first_button = self.driver.find_element(By.XPATH, "//li[contains(text(),'Cras justo odio')]")
         self.assertTrue(list_first_button.is_displayed(), 'List first button is not shown.')
@@ -33,12 +32,8 @@ class TestSelectable(BaseTest):
         list_first_button.click()
         self.assertTrue('active' in list_first_button.get_attribute('class'),    # verifies 'change' in class
                         'List first button is not selected after clicking on it.')
-        # asserts background color
         self.assertEqual(list_first_button.value_of_css_property('background-color'), 'rgba(0, 123, 255, 1)')
-        # prints color
-        print(f"color: {list_first_button.value_of_css_property('background-color')}")
-
-        # second sentence
+      
         list_second_button = self.driver.find_element(By.XPATH, "//li[contains(text(),'Dapibus ac facilisis in')]")
         self.assertTrue(list_second_button.is_displayed(), 'List second button is not shown')
         self.assertTrue('active' not in list_second_button.get_attribute('class'),
@@ -46,13 +41,8 @@ class TestSelectable(BaseTest):
         list_second_button.click()
         self.assertTrue('active' in list_second_button.get_attribute('class'),
                         'List second button is not selected after clicking on it.')
-
-        # asserts background color
         self.assertEqual(list_second_button.value_of_css_property('background-color'), 'rgba(0, 123, 255, 1)')
-        # prints color
-        print(f"color: {list_second_button.value_of_css_property('background-color')}")
-
-    # includes 3rd and 4th sentences
+        
     def test_selectable_text_is_displayed_2(self):
         list_third_button = self.driver.find_element(By.XPATH, "//li[contains(text(),'Morbi leo risus')]")
         self.assertTrue(list_third_button.is_displayed(), 'List second button is not shown')
@@ -62,9 +52,7 @@ class TestSelectable(BaseTest):
         self.assertFalse('active' not in list_third_button.get_attribute('class'),
                          'List third button is not selected after clicking on it.')
         self.assertEqual(list_third_button.value_of_css_property('background-color'), 'rgba(0, 123, 255, 1)')
-        print(f"color: {list_third_button.value_of_css_property('background-color')}")
-
-        # 4th sentence
+    
         list_forth_button = self.driver.find_element(By.XPATH, "//li[contains(text(),'Porta ac consectetur ac')]")
         self.assertTrue(list_forth_button.is_displayed(), 'List second button is not shown')
         self.assertFalse('active' in list_forth_button.get_attribute('class'),
@@ -73,8 +61,7 @@ class TestSelectable(BaseTest):
         self.assertFalse('active' not in list_forth_button.get_attribute('class'),
                          'List third button is not selected after clicking on it.')
         self.assertEqual(list_forth_button.value_of_css_property('background-color'), 'rgba(0, 123, 255, 1)')
-        print(f"color: {list_forth_button.value_of_css_property('background-color')}")
-
+        
     def test_selectable_grid_1(self):
         demo_tab_grid = self.driver.find_element(By.ID, "demo-tab-grid")
         self.assertTrue(demo_tab_grid.is_displayed())
