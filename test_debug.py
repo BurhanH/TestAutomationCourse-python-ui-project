034b0eb66c9d0ad8c5f590f8b4e66216b3ed93c2
Stan Aretinskiy <sr575sr@gmail.com>
2022-06-17 19:31:40 -0700

    Experiment update 3

--- test_debug.py
+++ test_debug.py
@@ -1,23 +1,18 @@
 import os
 import pytest
 import seedir as sd
-from resources.data import PROJECT_NAME
 from tests.test_download_upload_file import DOWNLOAD_FOLDER_PATH
 from utils.project_utils import get_project_path
 
 
 def test_debug_dir_structure():
     sd.seedir(os.getcwd())
+    print("*" * 50)
     print(f"current directory: {os.getcwd()}")
     print(f"DOWNLOAD_FOLDER_PATH: {DOWNLOAD_FOLDER_PATH}")
     resources_folder_path = os.path.join(get_project_path(), 'resources')
     print(f"'resources' folder exists ({resources_folder_path}) : {os.path.exists(resources_folder_path)}")
     downloads_folder_path = os.path.join(get_project_path(), 'resources', 'downloads_folder')
-    print(f"'downloads_folder' folder exists  ({downloads_folder_path}) : {os.path.exists(downloads_folder_path)}")
-    print("*" * 50)
-    resources_folder_path = os.path.join(get_project_path(), PROJECT_NAME, 'resources')
-    print(f"'resources' folder exists ({resources_folder_path}) : {os.path.exists(resources_folder_path)}")
-    downloads_folder_path = os.path.join(get_project_path(), PROJECT_NAME, 'resources', 'downloads_folder')
     print(f"'downloads_folder' folder exists  ({downloads_folder_path}) : {os.path.exists(downloads_folder_path)}")
 
 
