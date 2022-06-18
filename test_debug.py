1fe067c19c82a6b405dd15ebe57278c7baaaea09
Stan Aretinskiy <sr575sr@gmail.com>
2022-06-17 19:17:25 -0700

    Experiment update 2

--- test_debug.py
+++ test_debug.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 import seedir as sd
+from resources.data import PROJECT_NAME
 from tests.test_download_upload_file import DOWNLOAD_FOLDER_PATH
 from utils.project_utils import get_project_path
 
@@ -12,6 +13,11 @@ def test_debug_dir_structure():
     resources_folder_path = os.path.join(get_project_path(), 'resources')
     print(f"'resources' folder exists ({resources_folder_path}) : {os.path.exists(resources_folder_path)}")
     downloads_folder_path = os.path.join(get_project_path(), 'resources', 'downloads_folder')
+    print(f"'downloads_folder' folder exists  ({downloads_folder_path}) : {os.path.exists(downloads_folder_path)}")
+    print("*" * 50)
+    resources_folder_path = os.path.join(get_project_path(), PROJECT_NAME, 'resources')
+    print(f"'resources' folder exists ({resources_folder_path}) : {os.path.exists(resources_folder_path)}")
+    downloads_folder_path = os.path.join(get_project_path(), PROJECT_NAME, 'resources', 'downloads_folder')
     print(f"'downloads_folder' folder exists  ({downloads_folder_path}) : {os.path.exists(downloads_folder_path)}")
 
 
