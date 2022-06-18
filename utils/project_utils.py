8bebdfdd983ed601b9d53d1c997e258d423abb33
Stan Aretinskiy <sr575sr@gmail.com>
2022-06-17 19:32:28 -0700

    Update utils/project_utils.py

--- utils/project_utils.py
+++ utils/project_utils.py
@@ -3,5 +3,10 @@ from resources.data import PROJECT_NAME
 
 
 def get_project_path():
-    return os.getcwd().split(PROJECT_NAME)[0] + PROJECT_NAME
-
+    cwd = os.getcwd()
+    # local setup
+    if cwd.count(PROJECT_NAME) == 1:
+        return os.path.join(cwd.split(PROJECT_NAME)[0], PROJECT_NAME)
+    # GutHub Actions setup
+    else:
+        return os.path.join(cwd.split(PROJECT_NAME)[0], PROJECT_NAME, PROJECT_NAME)
