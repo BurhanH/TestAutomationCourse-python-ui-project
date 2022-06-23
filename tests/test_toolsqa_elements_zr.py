4a8bbb6c2156bb8ea82f59b993c5d6086f8b948f
Keldieva <56282112+Keldieva@users.noreply.github.com>
2022-06-23 16:12:46 -0400

    changed errors

--- tests/test_toolsqa_elements_zr.py
+++ tests/test_toolsqa_elements_zr.py
@@ -155,9 +155,11 @@ class TestDemoqaRedirection(BaseTest):
     def test_download(self):
         self.driver.find_element(By.XPATH, "//div[@class='header-wrapper']/*[text()='Elements']").click()
         self.driver.execute_script("window.scrollBy(0,925)")
-        self.driver.find_element(By.XPATH, "//span[contains(text(), 'Upload and Download')]").click()
+        download_element = self.driver.find_element(By.XPATH, "//span[contains(text(), 'Upload and Download')]")
+        download_element.click()
         self.assertEqual(self.driver.current_url, DOWNLOAD_URL)
-        self.driver.find_element(By.CSS_SELECTOR, "#downloadButton").click()
+        download_button = self.driver.find_element(By.CSS_SELECTOR, "#downloadButton")
+        download_button.click()
 
 
 
